ec20a553bd9b87bdc316d7034d7f4cd6ce517bb1
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-03-16 23:04:44 -0500

    Reaction Model

---
+++ models/Reaction.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function (createdAt) {
+            return new Date(createdAt).toLocaleString();
+        },
+    },
+});
+
+module.exports = reactionSchema;
